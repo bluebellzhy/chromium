b0093ed744e0d8f51364224443b4c7f6c3b598c0
eseidel@google.com <eseidel@google.com@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2008-08-14 20:48:16 +0000

    Everyone lovs 1MB file limits.  TBR(ojan)
    git-svn-id: http://src.chromium.org/svn/branches/chrome_webkit_merge_branch@886 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- webkit/build/JSConfig/create-config.sh
+++ webkit/build/JSConfig/create-config.sh
@@ -58,6 +58,7 @@ fi
 #
 mkdir -p $JSHeadersDir
 JavaScriptCoreSrcDir="../../../third_party/WebKit/JavaScriptCore"
+WebCoreSrcDir="../../../third_party/WebKit/WebCore"
 if [[ "$2" = "kjs" ]]
 then
   mkdir -p $JSHeadersDir/JavaScriptCore
@@ -72,21 +73,21 @@ then
   $CP $JavaScriptCoreSrcDir/API/JSContextRef.h $JSHeadersDir/JavaScriptCore
   $CP $JavaScriptCoreSrcDir/API/JSStringRef.h $JSHeadersDir/JavaScriptCore
 
-  $CP $JavaScriptCoreSrcDir/bindings/npapi.h $JSHeadersDir
-  $CP $JavaScriptCoreSrcDir/bindings/npruntime.h $JSHeadersDir
-  $CP $JavaScriptCoreSrcDir/bindings/npruntime_internal.h $JSHeadersDir
-  $CP $JavaScriptCoreSrcDir/bindings/npruntime_impl.h $JSHeadersDir
-  $CP $JavaScriptCoreSrcDir/bindings/npruntime_priv.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npapi.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npruntime.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npruntime_internal.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npruntime_impl.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npruntime_priv.h $JSHeadersDir
 
-  $CP $JavaScriptCoreSrcDir/bindings/runtime.h $JSHeadersDir/JavaScriptCore
-  $CP $JavaScriptCoreSrcDir/bindings/np_jsobject.h $JSHeadersDir/JavaScriptCore
-  $CP $JavaScriptCoreSrcDir/bindings/runtime_object.h $JSHeadersDir/JavaScriptCore
-  $CP $JavaScriptCoreSrcDir/bindings/runtime_root.h $JSHeadersDir/JavaScriptCore
+  $CP $WebCoreSrcDir/bridge/runtime.h $JSHeadersDir/JavaScriptCore
+  $CP $WebCoreSrcDir/bridge/np_jsobject.h $JSHeadersDir/JavaScriptCore
+  $CP $WebCoreSrcDir/bridge/runtime_object.h $JSHeadersDir/JavaScriptCore
+  $CP $WebCoreSrcDir/bridge/runtime_root.h $JSHeadersDir/JavaScriptCore
 
   $CP $JavaScriptCoreSrcDir/kjs/JSImmediate.h $JSHeadersDir/JavaScriptCore
   $CP $JavaScriptCoreSrcDir/kjs/JSLock.h $JSHeadersDir/JavaScriptCore
   $CP $JavaScriptCoreSrcDir/kjs/JSType.h $JSHeadersDir/JavaScriptCore
-  $CP ../../../webkit/pending/kjs/collector.h $JSHeadersDir/JavaScriptCore
+  $CP $JavaScriptCoreSrcDir/kjs/collector.h $JSHeadersDir/JavaScriptCore
   $CP $JavaScriptCoreSrcDir/kjs/interpreter.h $JSHeadersDir/JavaScriptCore
   $CP $JavaScriptCoreSrcDir/kjs/protect.h $JSHeadersDir/JavaScriptCore
   $CP $JavaScriptCoreSrcDir/kjs/ustring.h $JSHeadersDir/JavaScriptCore
@@ -94,8 +95,8 @@ then
 
   $CP $JavaScriptCoreSrcDir/wtf/HashCountedSet.h $JSHeadersDir/JavaScriptCore
 else 
-  $CP $JavaScriptCoreSrcDir/bindings/npapi.h $JSHeadersDir
-  $CP $JavaScriptCoreSrcDir/bindings/npruntime.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npapi.h $JSHeadersDir
+  $CP $WebCoreSrcDir/bridge/npruntime.h $JSHeadersDir
   $CP ../../../webkit/port/bindings/v8/npruntime_priv.h $JSHeadersDir
 fi
 
