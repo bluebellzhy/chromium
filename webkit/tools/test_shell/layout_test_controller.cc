33f48d7ec0f770f5a954027261d225e2b41c59ea
tc@google.com <tc@google.com@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2008-09-29 16:42:30 +0000

    Bring the layout test plugin closer to the upstream plugin by updating PluginObject.cpp to be closer to the upstream version.  There's still more to be done here, but we need to upstream some tests.
    Update the layout test controller to be able to resolve
    files in /tmp/ (by using the windows temp directory).
    
    This moves a plugin test from timing out to failing.
    
    Review URL: http://codereview.chromium.org/4313
    
    git-svn-id: http://src.chromium.org/svn/branches/chrome_webkit_merge_branch@2672 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- webkit/tools/test_shell/layout_test_controller.cc
+++ webkit/tools/test_shell/layout_test_controller.cc
@@ -9,10 +9,11 @@
 #include "webkit/tools/test_shell/layout_test_controller.h"
 
 #include "base/basictypes.h"
+#include "base/file_util.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
-#include "base/string_util.h"
 #include "base/path_service.h"
+#include "base/string_util.h"
 #include "webkit/glue/webframe.h"
 #include "webkit/glue/webpreferences.h"
 #include "webkit/glue/webview.h"
@@ -475,6 +476,15 @@ void LayoutTestController::pathToLocalResource(
     return;
 
   std::string url = args[0].ToString();
+  if (StartsWithASCII(url, "/tmp/", true)) {
+    // We want a temp file.
+    std::wstring path;
+    PathService::Get(base::DIR_TEMP, &path);
+    file_util::AppendToPath(&path, UTF8ToWide(url.substr(5)));
+    result->Set(WideToUTF8(path));
+    return;
+  }
+
   // Some layout tests use file://// which we resolve as a UNC path.  Normalize
   // them to just file:///.
   while (StartsWithASCII(url, "file:////", false)) {
